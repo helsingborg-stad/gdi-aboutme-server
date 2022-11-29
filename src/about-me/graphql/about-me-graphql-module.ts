97933d2121a422783787c547a881759f483d052b
Joakim Larsson <joakim.larsson2@helsingborg.se>
2022-11-29 10:00:26 +0100

    🚑️ added actions to GraphQL schema

--- src/about-me/graphql/about-me-graphql-module.ts
+++ src/about-me/graphql/about-me-graphql-module.ts
@@ -19,6 +19,7 @@ export const createAboutMe = (services: AboutMeServices): GraphQLModule => ({
 			status: String
 			statusHint: String
 			events: [CaseEvent]
+			actions: [CaseAction]
 		}
 		type CaseEvent {
 			updateTime: String
