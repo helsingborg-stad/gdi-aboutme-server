013cdb85e0f9b0aad80f71dd9c43334499d6c7b6
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-02-09 10:46:45 +0100

    :baby: repository tests - a humble start

--- src/about-me/person/repositories/hbg-datatorget/__test/repository.test.ts
+++ src/about-me/person/repositories/hbg-datatorget/__test/repository.test.ts
@@ -1,6 +1,6 @@
 import { createDefaultPersonUpdater } from '../../../updaters/index'
 import { createHbgDatatorgetPersonRepository } from '../hbg-datatorget-person-respository'
-import { PersonInformation, RestClient } from '../rest-api'
+import { CATEGORY_PRIVATE, CONTACT_TYPE_EMAIL, CONTACT_TYPE_PHONE, PersonInformation, RestClient } from '../rest-api'
 
 const notImplemented = () => { throw new Error('not implemented') }
 
@@ -15,14 +15,14 @@ const typed = <T>(value: T): T => value
 
 describe('createHbgDatatorgetPersonRepository()', () => {
 	it('getPerson() delegates to REST api', async () => {
-		const fakeGetPerson = jest.fn().mockReturnValue(Promise.resolve(
+		const fakeGetPerson = jest.fn().mockResolvedValue(
 			typed<PersonInformation>({
 				person_id: 'pid-123',
 				social_security_number: 'ssn-123',
 				first_name: 'Tiger',
 				last_name: 'Stripes',
 				contact_information: [],
-			})))
+			}))
 		const repo = createRepo({
 			getPerson: fakeGetPerson,
 		})
@@ -37,8 +37,40 @@ describe('createHbgDatatorgetPersonRepository()', () => {
 		expect(fakeGetPerson).toHaveBeenCalledTimes(1)
 	})
 
+	it('updatePerson()', async () => {
+		const fakeGetPerson = jest.fn().mockResolvedValue(
+			typed<PersonInformation>({
+				person_id: 'pid-123',
+				social_security_number: '',
+				first_name: 'John',
+				last_name: 'Doe',
+				contact_information: [],
+			}))
+		const fakeUpdateContactDetails = jest.fn()
+
+		const repo = createRepo({
+			getPerson: fakeGetPerson,
+			updateContactDetails: fakeUpdateContactDetails,
+		})
+
+		await repo.updatePerson('pid-123', { email: 'a@b.com', phoneNumber: '123-456' })
+
+		expect(fakeUpdateContactDetails).toHaveBeenCalledWith('pid-123', [
+			{
+				category_id: CATEGORY_PRIVATE,
+				contact_type_id: CONTACT_TYPE_EMAIL,
+				contact_type_value: 'a@b.com',
+			},
+			{
+				category_id: CATEGORY_PRIVATE,
+				contact_type_id: CONTACT_TYPE_PHONE,
+				contact_type_value: '+46123456',
+			},
+		])
+	})
+
 	it('verifyEmail() -> verifyContactDetails()', async () => {
-		const fakeVerifyContactDetails = jest.fn().mockReturnValueOnce(Promise.resolve({ firstName: 'Tiger' }))
+		const fakeVerifyContactDetails = jest.fn().mockResolvedValueOnce({ firstName: 'Tiger' })
 		const repo = createRepo({
 			verifyContactDetails: fakeVerifyContactDetails,
 		})
@@ -48,7 +80,7 @@ describe('createHbgDatatorgetPersonRepository()', () => {
 	})
 
 	it('verifyPhone() -> verifyContactDetails()', async () => {
-		const fakeVerifyContactDetails = jest.fn().mockReturnValueOnce(Promise.resolve({ firstName: 'Tiger' }))
+		const fakeVerifyContactDetails = jest.fn().mockResolvedValueOnce({ firstName: 'Tiger' })
 		const repo = createRepo({
 			verifyContactDetails: fakeVerifyContactDetails,
 		})
