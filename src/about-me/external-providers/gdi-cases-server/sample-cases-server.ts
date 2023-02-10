ed151c17d42fc573530afeca37549dbbc2b96581
Joakim Larsson <joakim.larsson2@helsingborg.se>
2023-02-10 16:33:31 +0100

    :beer: Everyone has (example) cases...

---
+++ src/about-me/external-providers/gdi-cases-server/sample-cases-server.ts
@@ -0,0 +1,49 @@
+import { GdiCasesServer, GdiCasesServerCase } from './types'
+
+const makeCase = (subjectId: string, i: number, status: string, statusHint): GdiCasesServerCase => ({
+	caseId: `sample-case-${i}`,
+	subjectId,
+	updateTime: '2023-01-01',
+	organization: 'Exempelförvaltningen',
+	label: 'Du har ett exempelärende',
+	description: `Detta är ett exempelärende hos exempelförvaltningen. Lite simulerade exempeldetaljer är att ditt personnummer är ${subjectId} och det påhittade ärendenumret är ${i}.`,
+	status,
+	statusHint,
+	events: [ {
+		updateTime: new Date().toISOString(),
+		label: 'Uppdatering',
+		description: 'En påhittad exempelhandläggare uppdaterade detta exempelärende alldeles nyss',
+		status: 'Uppdaterad',
+		statusHint: '',
+		actions: [{
+			label: 'Visa mitt ärende i exempel-e-tjänsten',
+			typeHint: 'link',
+			url: 'https://www.example.com',
+		}],
+	}, {
+		updateTime: '2023-01-01',
+		label: 'Inkommet',
+		description: 'Ditt exempelärende är registerat i vårt exempelverksamhessystem',
+		status: 'Inkommet',
+		statusHint: '',
+		actions: [],
+	} ],
+	actions: [{
+		label: 'Visa mitt ärende i exempel-e-tjänsten',
+		typeHint: 'link',
+		url: 'https://www.example.com',
+	}],
+})
+
+const createSampleCases = (subjectId: string): GdiCasesServerCase[] => ([
+	makeCase(subjectId, 101, 'Godkänt', 'approved'),
+	makeCase(subjectId, 202, 'Nekad', 'rejected'),
+	makeCase(subjectId, 303, 'Avslutat', 'closed'),
+	makeCase(subjectId, 404, 'Komplettering krävs', 'incomplete'),
+])
+
+export const createSampleGdiCasesServer = (inner: GdiCasesServer): GdiCasesServer => ({
+	listCasesBySubjectId: async (subjectId) => inner
+		.listCasesBySubjectId(subjectId)
+		.then(cases => cases?.length > 0 ? cases : createSampleCases(subjectId)),
+})
